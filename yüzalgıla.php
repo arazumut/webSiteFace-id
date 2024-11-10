8db28b161c6922ed432449c4e697349b754e22f3
K.Umut Araz <150933483+arazumut@users.noreply.github.com>
2024-11-10 15:25:43 +0300

    Update yüzalgıla.php

--- yüzalgıla.php
+++ yüzalgıla.php
@@ -11,6 +11,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
 </head>
+
 <style>
     
     @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;400;500;600&display=swap');
